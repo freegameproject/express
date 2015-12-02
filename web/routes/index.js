30e0ee5c1491b773ca9c579af044cd16ec3a710e
freegameproject <freegameproject@sina.com>
2015-12-02 11:43:36 +0800

    ...

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,22 +6,40 @@ var ObjectId = require('mongodb').ObjectID;
 mongoUrl = 'mongodb://localhost:27017/web';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  MongoClient.connect(mongoUrl, function(err, db) {
-    assert.equal(null, err);
-    findRestaurants(db, function(doc) {
-      res.render('index', { title: ' node ' ,doc:doc});
-      db.close();
+router.get('/', function (req, res, next) {
+    MongoClient.connect(mongoUrl, function (err, db) {
+        assert.equal(null, err);
+        findRestaurants(db, function (doc) {
+            res.render('index', {title: ' node ', doc: doc});
+            db.close();
+        });
     });
-  });
+});
+/* GET blog page. */
+router.get('/blog', function (req, res, next) {
+    var id = req.query.id;
+    MongoClient.connect(mongoUrl, function (err, db) {
+        assert.equal(null, err);
+        findDocById(db, id, function (doc) {
+            res.render('blog', {title: ' node ', doc: doc[0]});
+            db.close();
+        });
+    });
 });
 
 
-var findRestaurants = function(db, callback) {
-  db.collection('blogs').find({}).toArray(function(err,arr){
-    callback(arr);
-    console.dir(arr);
-  });
+var findRestaurants = function (db, callback) {
+    db.collection('blogs').find({}).toArray(function (err, arr) {
+        callback(arr);
+        console.dir(arr);
+    });
+};
+
+var findDocById = function (db, id, callback) {
+    db.collection('blogs').find({'_id': ObjectId(id)}).toArray(function (err, arr) {
+        callback(arr);
+        console.dir(arr);
+    });
 };
 
 module.exports = router;
