7f642578815feb0cd3115d3226643ce938b0f51e
freegameproject <freegameproject@sina.com>
2015-12-08 12:09:07 +0800

    ...

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,12 +6,8 @@ MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 mongoUrl = 'mongodb://localhost:27017/web';
-//var crypto = require('crypto');
-//var md5=crypto.createHash('md5');
 var md5 = require('md5');
 var session = require('express-session');
-
-
 router.use(session({
     secret: 'wb',
     resave: false,
@@ -42,7 +38,7 @@ router.get('/blog/:id', function (req, res, next) {
             var admin = req.session.admin;
             if (admin === true) {
                 res.render('admin_blog', {title: doc[0].title, doc: doc[0]});
-            }else{
+            } else {
                 res.render('blog', {title: doc[0].title, doc: doc[0]});
             }
             db.close();
@@ -188,11 +184,7 @@ router.post('/uploadimg', function (req, res, next) {
     form.parse(req, function (err, fields, files) {
         var imgs = [];
         for (var key in files) {
-
-
             var file = files[key];
-
-
             //var fName = (new Date()).getTime();
             var fName = md5(fs.readFileSync(file.path));
 
@@ -207,11 +199,6 @@ router.post('/uploadimg', function (req, res, next) {
                     fName = fName + ".png";
                     break;
             }
-            //console.log("SIZE:"+file.size);
-
-            //var uploadDir=path.resolve('public', 'upload')+fName;
-
-            //console.log(uploadDir);
             var uploadDir = path.resolve('public', 'upload', fName);
 
             imgs.push(fName);
@@ -223,23 +210,9 @@ router.post('/uploadimg', function (req, res, next) {
 
                 }
             });
-            //fs.renameSync(files.upload.path,"public/upload/"+ fName);
         }
         res.json(imgs);
     });
-    /*
-     form.encoding = 'utf-8';		//设置编辑
-     form.uploadDir = 'public' + AVATAR_UPLOAD_FOLDER;	 //设置上传目录
-     form.keepExtensions = true;	 //保留后缀
-     form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
-     form.parse(req, function (error, fields, files) {
-     var types = files.upload.name.split('.');
-     var date = new Date();
-     var ms = Date.parse(date);
-     fs.renameSync(files.upload.path, "/tmp/files" + ms + "." + String(types[types.length - 1]));
-     });
-     */
-    //
 });
 
 
